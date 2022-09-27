d0b81fd0c5e1ff1c88ebb4afd2a1daac1623b2ed
Mario <mariogr901@gmail.com>
2022-09-27 20:01:57 +0200

    Comentario agregado.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,9 @@ import org.jetbrains.kotlinx.dataframe.api.cast
 import org.jetbrains.kotlinx.dataframe.api.print
 import org.jetbrains.kotlinx.dataframe.io.readCSV
 
+
+// RECORDATORIO
+// Implementar siguiente linea en build.gradle.kts ===  implementation("org.jetbrains.kotlinx:dataframe:0.8.1") en "dependencies"
 fun main() {
 
     val fileToRead = DataFrame.readCSV("./data/products.csv")
